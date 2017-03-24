b8c3e3c0e2b1071e1163c767f1f6ad0de8322a13
qizl <q@qizl.cn>
2017-03-25 00:41:51 +0800

    -

---
+++ WebClient/WebClient/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public string File()
+        {
+            if (Request.Files != null && Request.Files.Count > 0)
+            {
+                // 1.保存文件
+                var f = Request.Files[0];
+                string fileName = Path.GetFileName(f.FileName);
+
+                //Response.AddHeader("location", $"http://localhost:62299/{fileName}");
+                //Response.Write(fileName);
+
+                return fileName;
+            }
+            else throw new Exception("未接收到文件！");
+        }
+    }
+}
---
+++ WebServer/WebServer/Controllers/FileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Http;
+
+namespace WebServer.Controllers
+{
+    public class FileController : ApiController
+    {
+        public string Post()
+        {
+            var context = (HttpContextBase)Request.Properties["MS_HttpContext"];
+
+            if (context.Request.Files != null && context.Request.Files.Count > 0)
+            {
+                // 1.保存文件
+                var f = context.Request.Files[0];
+                string fileName = Path.GetFileName(f.FileName);
+
+                return fileName;
+            }
+            throw new Exception("未接收到文件！");
+        }
+    }
+}
